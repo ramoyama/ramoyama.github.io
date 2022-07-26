d2a38a30d76b202a058aa8a95b2e9571e0233887
ramoyama <92776750+ramoyama@users.noreply.github.com>
2022-07-26 13:57:23 +0900

    Update color.js

--- color.js
+++ color.js
@@ -1,4 +1,10 @@
 const target = document.getElementById('hoge');
+const imgtaget = document.getElementById('hogea');
+imgtaget.style.display ="none";
 function colorchange(){
   target.style.color ="red";
+}
+
+function imgget(){
+  imgtaget.style.display = "flex";
 }
